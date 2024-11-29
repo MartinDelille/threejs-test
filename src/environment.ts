afe0304c0dc6b2f5d163c7975eb87c67f0072104
Martin Delille <martin@delille.org>
2024-11-29 17:44:54 +0100

    Add wave effect to water

--- src/environment.ts
+++ src/environment.ts
@@ -10,7 +10,7 @@ export class Environment {
   private _renderTarget: THREE.WebGLRenderTarget | undefined;
 
   constructor(private scene: THREE.Scene, private renderer: THREE.WebGLRenderer) {
-    const waterGeometry = new THREE.PlaneGeometry(10000, 10000);
+    const waterGeometry = new THREE.PlaneGeometry(1000, 1000, 1000, 1000);
 
     this._water = new Water(waterGeometry, {
       textureWidth: 512,
@@ -72,6 +72,20 @@ export class Environment {
 
     this.scene.environment = this._renderTarget.texture;
 
+    const vertices = this._water.geometry.attributes.position.array;
+    const waveAngle = Math.PI / 9;
+    const cosAngle = Math.cos(waveAngle);
+    const sinAngle = Math.sin(waveAngle);
+
+    for (let i = 0; i < vertices.length; i += 3) {
+      const x = vertices[i];
+      const y = vertices[i + 1];
+      const period = 0.05;
+      const rotatedX = x * cosAngle - y * sinAngle;
+      vertices[i + 2] = Math.sin(rotatedX * period + time) * 4;
+    }
+
+    this._water.geometry.attributes.position.needsUpdate = true;
     this._water.material.uniforms.time.value += 1.0 / 60.0;
   }
 }
