f464544ffa56f68aa5a8bc14cbb0fbc882eacf62
Martin Delille <martin@delille.org>
2024-12-19 14:48:08 +0100

    Add @types/three to devDependencies

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import * as THREE from "three";
 
-import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import * as CANNON from "cannon-es";
 import * as Tone from "tone";
 
